708760118285e9d38feea6925b69d3378ed66cc6
Peyman Salehi <peymanslh@pm.me>
2020-10-29 17:55:39 +0330

    add comment for get data func

--- buying/data.py
+++ buying/data.py
@@ -1,6 +1,11 @@
 import json
 
 def get_data():
+    """
+        Get products list from json file
+        args:
+        return []products
+    """
     with open('buying/products.json') as f:
         return json.load(f)
 
